99e1d35e003639d8444ff905f4971ce2247b855a
dlagm <dlagm@DESKTOP-HK82DOS>
2023-01-02 23:05:58 +0900

    edit DAO, Service

--- newbalance/src/main/java/member/dao/MemberDAO.java
+++ newbalance/src/main/java/member/dao/MemberDAO.java
@@ -117,56 +117,100 @@ public class MemberDAO implements IMember {
 	}
 
 	// 회원가입
-	public int insert(Connection conn, MemberDTO mem) throws SQLException {
-		System.out.println("dao : " + mem.toString());
-		PreparedStatement pstmt = null;
-		String sql = " insert into user_info(" + "user_code, user_id, user_pwd, " + "user_gender, user_skt, user_phone,"
-				+ "user_name, user_birth, user_nativecheck," + "user_tel, user_receiveinfo, user_specialdate"
-				+ " ,user_email ,user_zipcode, user_insertdate,"
-				+ " user_state, user_friendcode, user_address1,user_address2) "
+	   public int insert(Connection conn, MemberDTO mem) throws SQLException {
+	      System.out.println("dao : "+mem.toString());
+	      PreparedStatement pstmt = null;
+	      String sql = " insert into user_info("
+	            + "user_code, user_id, user_pwd, "
+	            + "user_gender, user_skt, user_phone,"
+	            + "user_name, user_birth, user_nativecheck,"
+	            + "user_tel, user_receiveinfo, user_specialdate"
+	            + " ,user_email ,user_zipcode, user_insertdate,"
+	            + " user_state, user_friendcode, user_address1,user_address2) "
+	            
+	            + "values('M'||trim(to_char(user_Seq1.nextval)),?,?,?,?,?,?,?,?,?,?,?,?,?,SYSDATE,1,DBMS_RANDOM.STRING('X',10),?,?) ";
+	      try {
+	         pstmt = conn.prepareStatement(sql);
+	            pstmt.setString(1, mem.getUserId());
+	            pstmt.setString(2, mem.getUserPwd());
+	            pstmt.setInt(3, mem.getUserGender());
+	            pstmt.setString(4, mem.getUserSkt());
+	            pstmt.setString(5, mem.getUserPhone());
+	            pstmt.setString(6, mem.getUserName());
+	            pstmt.setString(7, mem.getUserBirth());
+	            pstmt.setInt(8, mem.getUserNativecheck());
+	            pstmt.setString(9, mem.getUserTel());
+	            pstmt.setInt(10, mem.getUserReceiveinfo());
+	            pstmt.setString(11, mem.getUserSpecialdate());
+	            pstmt.setString(12, mem.getUserEmail());
+	            pstmt.setInt(13, mem.getUserZipcode());
+	            pstmt.setString(14, mem.getUserAddress1());
+	            pstmt.setString(15, mem.getUserAddress2());
+	         
+	         return pstmt.executeUpdate(); 
+	      } finally {
+	         JdbcUtil.close(pstmt);
+	      }
+	   }
 
-				+ "values('M'||trim(to_char(user_Seq1.nextval)),?,?,?,?,?,?,?,?,?,?,?,?,?,SYSDATE,1,DBMS_RANDOM.STRING('X',10),?,?) ";
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, mem.getUserId());
-			pstmt.setString(2, mem.getUserPwd());
-			pstmt.setInt(3, mem.getUserGender());
-			pstmt.setString(4, mem.getUserSkt());
-			pstmt.setString(5, mem.getUserPhone());
-			pstmt.setString(6, mem.getUserName());
-			pstmt.setString(7, mem.getUserBirth());
-			pstmt.setInt(8, mem.getUserNativecheck());
-			pstmt.setString(9, mem.getUserTel());
-			pstmt.setInt(10, mem.getUserReceiveinfo());
-			pstmt.setString(11, mem.getUserSpecialdate());
-			pstmt.setString(12, mem.getUserEmail());
-			pstmt.setInt(13, mem.getUserZipcode());
-			pstmt.setString(14, mem.getUserAddress1());
-			pstmt.setString(15, mem.getUserAddress2());
+	   
+	   //회원 수정
+	   
+	      public int update(Connection con, MemberDTO myInfo) throws SQLException { 
+	         PreparedStatement pstmt = null;
 
-			return pstmt.executeUpdate();
-		} finally {
-			JdbcUtil.close(pstmt);
-		}
-	}
+	         String sql="update user_info "
+	               + "set  user_pwd = ? , user_phone=?,user_tel = ?, user_recelveinfo = ?,user_spacialdate = ?, user_email = ? , "
+	               + "user_zipcode= ?,user_address1 = ?, user_address2 = ? where user_code = ?";
+	             
+	         try {
+	            pstmt = con.prepareStatement(sql);
+	            pstmt.setString(1, myInfo.getUserPwd());
+	            pstmt.setString(2, myInfo.getUserPhone());
+	            pstmt.setString(3, myInfo.getUserTel());
+	            pstmt.setInt   (4, myInfo.getUserReceiveinfo());
+	            pstmt.setString(5, myInfo.getUserSpecialdate());
+	            pstmt.setString(6, myInfo.getUserEmail());
+	            pstmt.setInt(7, myInfo.getUserZipcode());
+	            pstmt.setString(8, myInfo.getUserAddress1());
+	            pstmt.setString(9, myInfo.getUserAddress2());
+	            pstmt.setString(10, myInfo.getUserCode());
+	            int rowCount = pstmt.executeUpdate();
+	            System.out.println("안녕"+rowCount);
+	            return rowCount;
+	         }finally {
+	            JdbcUtil.close(pstmt);
+	         }
+	      }
 
-	// 회원 수정
-	@Override
-	public int update(Connection con, MemberDTO mem) throws SQLException {
-		PreparedStatement pstmt = null;
-
-		String sql = "update user_info " + "set message=?, guest_name = ?" + "where message_id=? and password=?";
-		try {
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, message.getMessage());
-			pstmt.setString(2, message.getGuest_name());
-			pstmt.setInt(3, message.getMessage_id());
-			pstmt.setString(4, message.getPassword());
-			return pstmt.executeUpdate();
-		} finally {
-			JdbcUtil.close(pstmt);
-			JdbcUtil.close(con);
-		}
+	   public MemberDTO getMemberInfo(Connection conn, String userCode) throws SQLException {
+	      String sql = "SELECT * FROM user_info WHERE user_code = ?";
+	            
+	      
+	      PreparedStatement pstmt = null;
+	      ResultSet rs = null;
+	      try {
+	         pstmt=conn.prepareStatement(sql);
+	         pstmt.setString(1, userCode);
+	         rs = pstmt.executeQuery();
+	         
+	         MemberDTO myInfo = null;
+	         if (rs.next()) {
+	            myInfo =  new MemberDTO(rs.getString("user_code"), rs.getString("user_id"),rs.getString("user_pwd"),
+	                  rs.getInt("user_gender"), rs.getString("user_skt"), rs.getString("user_phone"),
+	                  rs.getString("user_name"), rs.getString("user_birth"), rs.getInt("user_nativecheck"),
+	                  rs.getString("user_tel"), rs.getInt("user_receiveinfo"), rs.getString("user_specialdate"),
+	                  rs.getString("user_email"), rs.getInt("user_zipcode"), rs.getString("user_insertdate"),
+	                  rs.getInt("user_state"), rs.getInt("user_mileage"), rs.getString("user_friendcode"),
+	                  rs.getString("user_lastlogin"), rs.getString("user_address1"),rs.getString("user_address2"), rs.getString("lv"));
+	      
+	         }
+	         
+	         return myInfo;
+	      }finally {
+	         JdbcUtil.close(rs);
+	         JdbcUtil.close(pstmt);
+	      }
 	}
 
 
