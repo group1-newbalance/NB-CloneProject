c9be35373149f14f6b2b7a39a886b686b493e4db
dlagm <dlagm@DESKTOP-HK82DOS>
2023-01-07 19:17:57 +0900

    Edit BuyProductDTO, OrderHandler

--- newbalance/src/main/java/product/domain/BuyProductDTO.java
+++ newbalance/src/main/java/product/domain/BuyProductDTO.java
@@ -6,15 +6,49 @@ public class BuyProductDTO {
 	private int pdPrice;
 	private String pdSize;
 	private int pdAmount;
+	private String pdColor;
+	private int colorCode;
+	private String pdImage;
+	private String pdName;
+
 	
-	public BuyProductDTO(String pdCode, int pdPrice, String pdSize, int pdAmount) {
+	public BuyProductDTO(String pdCode, int pdPrice, String pdSize, int pdAmount, String pdColor, int colorCode,
+			String pdImage, String pdName) {
 		super();
 		this.pdCode = pdCode;
 		this.pdPrice = pdPrice;
 		this.pdSize = pdSize;
 		this.pdAmount = pdAmount;
+		this.pdColor = pdColor;
+		this.colorCode = colorCode;
+		this.pdImage = pdImage;
+		this.pdName = pdName;
 	}
 	
+	public String getPdColor() {
+		return pdColor;
+	}
+	public void setPdColor(String pdColor) {
+		this.pdColor = pdColor;
+	}
+	public int getColorCode() {
+		return colorCode;
+	}
+	public void setColorCode(int colorCode) {
+		this.colorCode = colorCode;
+	}
+	public String getPdImage() {
+		return pdImage;
+	}
+	public void setPdImage(String pdImage) {
+		this.pdImage = pdImage;
+	}
+	public String getPdName() {
+		return pdName;
+	}
+	public void setPdName(String pdName) {
+		this.pdName = pdName;
+	}
 	public String getPdCode() {
 		return pdCode;
 	}
